200f2c9731116214296101ce34b53be0080797d8
Viktorija <gulbickaja.viktorija@gmail.com>
2019-05-07 14:17:27 +0300

    add hamburger menu, not work yet

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,16 +1,5 @@
 $(document).ready(function(){
 
-// education/course section - toggle certifications
-	// $(".toggle-btn").click(function(){
-	// 	$(".img-holder", this).toggleClass("open", 5000);
-	// 	$(".arrow-right", this).toggleClass("change-icon");
-	// 	$(this).siblings(".toggle-btn").slideToggle();
-	// });
-
-// open magnific popup
-	//   $(".open-popup-link").magnificPopup({
-	//     type:'inline'
-	// });
 	$('.image-popup-vertical-fit').magnificPopup({
 		type: 'image',
 		closeOnContentClick: true,
