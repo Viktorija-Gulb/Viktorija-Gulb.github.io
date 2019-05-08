b9dce996795168f0739c1331f9a952057d1235f0
Viktorija <gulbickaja.viktorija@gmail.com>
2019-05-08 09:50:47 +0300

    open-close menu on mobile

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,16 +1,47 @@
 $(document).ready(function(){
 
-	$('.image-popup-vertical-fit').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		mainClass: 'mfp-img-mobile',
-		image: {
-			verticalFit: true
-		}
+	// $('.image-popup-vertical-fit').magnificPopup({
+	// 	type: 'image',
+	// 	closeOnContentClick: true,
+	// 	mainClass: 'mfp-img-mobile',
+	// 	image: {
+	// 		verticalFit: true
+	// 	}
 		
-	});
+	// });
 
 
+	// hamburger menu
+
+	var mq = window.matchMedia("(max-width: 799px)");
+
+  var processMq = function(){
+      if (mq.matches) {
+      	$(".hamburger").click(function(){
+					$(".nav-menu").css("display", "block");
+				});
+  	
+      } 
+      else{
+
+      }
+
+      if(mq.matches) {
+      	$(".close").click(function(){
+         	$(".nav-menu").removeAttr("style");
+          console.log("click work");
+        });
+      }
+  };
+
+  // step 1 - on load
+  
+  processMq();
+
+   // on resize
+  mq.addListener(function(mql){
+      processMq();
+  });
 
 });
 
