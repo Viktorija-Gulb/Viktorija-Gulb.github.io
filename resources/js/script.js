40a745bc8a2626cc841cd9027ce3da2bf6261923
Viktorija <gulbickaja.viktorija@gmail.com>
2019-05-09 10:25:01 +0300

    wip with popups

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
 
-	// $('.image-popup-vertical-fit').magnificPopup({
-	// 	type: 'image',
-	// 	closeOnContentClick: true,
-	// 	mainClass: 'mfp-img-mobile',
-	// 	image: {
-	// 		verticalFit: true
-	// 	}
+	$('.image-popup-vertical-fit').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-img-mobile',
+		image: {
+			verticalFit: true
+		}
 		
-	// });
+	});
+
 
 
 	// hamburger menu
@@ -19,6 +20,8 @@ $(document).ready(function(){
       if (mq.matches) {
       	$(".hamburger").click(function(){
 					$(".nav-menu").css("display", "flex");
+					$(this).css("display", "none");
+					$(".title").css("display", "none");
 				});
   	
       } 
@@ -29,6 +32,9 @@ $(document).ready(function(){
       if(mq.matches) {
       	$(".close").click(function(){
          	$(".nav-menu").removeAttr("style");
+         	$(".hamburger").removeAttr("style");
+         	$(".title").removeAttr("style");
+         	
           console.log("click work");
         });
       }
