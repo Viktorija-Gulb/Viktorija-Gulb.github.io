a30f6f85c867208d85b221a1dce18a3f8b3c0ab5
Viktorija <gulbickaja.viktorija@gmail.com>
2019-07-09 10:51:46 +0300

    change work section, change footer background

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,61 +1,73 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  // magnific popups in work section
 
-	// magnific popups in work section
-
-	$('.open-popup-link').magnificPopup({
-	  type:'inline',
-	  midClick: true // 
-	});
-
-	// magnific popups in education section
-
-	$('.image-popup-vertical-fit').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		mainClass: 'mfp-img-mobile',
-		image: {
-			verticalFit: true
-		}
-		
-	});
+  $(".open-popup-link").magnificPopup({
+    type: "inline",
+    midClick: true //
+  });
 
 
-	// hamburger menu
+  // magnific popups in education section
 
-	var mq = window.matchMedia("(max-width: 768px)");
+  $(".image-popup-vertical-fit").magnificPopup({
+    type: "image",
+    closeOnContentClick: true,
+    mainClass: "mfp-img-mobile",
+    image: {
+      verticalFit: true
+    }
+  });
 
-  var processMq = function(){
-      if (mq.matches) {
-      	$(".hamburger").click(function(){
-					$(".nav-menu").css("display", "flex");
-					$(this).css("display", "none");
-					$(".title").css("display", "none");
-				});
-      } 
-      else{
+  // hamburger menu
+
+  var mq = window.matchMedia("(max-width: 768px)");
+
+  var processMq = function() {
+    if (mq.matches) {
+      $(".hamburger").click(function() {
+        $(".nav-menu").css("display", "flex");
+        $(this).css("display", "none");
+        $(".title").css("display", "none");
+      });
+    } else {
+      $(".title").removeAttr("style");
+    }
+
+    if (mq.matches) {
+      $(".close").click(function() {
+        $(".nav-menu").removeAttr("style");
+        $(".hamburger").removeAttr("style");
         $(".title").removeAttr("style");
-      }
-
-      if(mq.matches) {
-      	$(".close").click(function(){
-         	$(".nav-menu").removeAttr("style");
-         	$(".hamburger").removeAttr("style");
-         	$(".title").removeAttr("style");
-        });
-      }else {
-        
-      }
+      });
+    } else {
+    }
   };
 
   // step 1 - on load
-  
+
   processMq();
 
-   // on resize
-  mq.addListener(function(mql){
-      processMq();
+  // on resize
+  mq.addListener(function(mql) {
+    processMq();
   });
-
 });
 
 
+// hover effect in work section
+
+  // const hideContent = document.querySelector(".hide-content");
+  // const productBox = document.querySelector(".product-box");
+  // let showText = false;
+
+  // productBox.addEventListener("mouseover", viewText);
+
+  // function viewText() {
+  //   if(!showText) {
+  //     hideContent.classList.add("show");
+  //     showText = true;
+  //   } else {
+  //     hideContent.classList.remove("show");
+  //     showText = false;
+  //   }
+  // }
